e96e550fa507fcf5ab4e91158f1110f9b60d4d3e
SuperBaggers <94622317+SuperBaggers@users.noreply.github.com>
2026-06-15 19:01:25 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 //  Paste your Spotify Client ID between the quotes (from the Spotify
 //  developer dashboard). That's the only thing you need to change.
 // ===========================================================================
-window.SPOTIFY_CLIENT_ID = "PASTE_YOUR_CLIENT_ID_HERE";
+window.SPOTIFY_CLIENT_ID = "9aa82d06086945ad9ba25238fd40bb56";
